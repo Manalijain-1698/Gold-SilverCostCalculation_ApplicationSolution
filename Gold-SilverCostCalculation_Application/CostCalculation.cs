1449b2d5fc0a03ecdf1e5328c2c82ae29403c6c2
Manali Jain <Manali.Jain@kanini.com>
2021-05-13 17:54:15 +0530

    Add project files.

---
+++ Gold-SilverCostCalculation_Application/CostCalculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold_SilverCostCalculation_Application
+{
+    interface Gold
+    {
+        void GoldCostcalculation(double weight);
+    }
+
+    interface Silver
+    {
+        void SilverCostcalculation(double weight);
+
+    }
+
+    public class CostCalculation : Gold,Silver
+    {
+        public void GoldCostcalculation(double weight)
+        {
+            double currentgoldprice = 4900;
+            double totalamount = weight * currentgoldprice;
+            Console.WriteLine("Total Amount for Gold of weight {0} is {1}", weight, totalamount);
+        }
+
+        public void SilverCostcalculation(double weight)
+        {
+            double currentsilverprice = 75;
+            double totalamount = weight * currentsilverprice;
+            Console.WriteLine("Total Amount for Silver of weight {0} is {1}", weight, totalamount);
+        }
+
+        
+    }
+}
---
+++ Gold-SilverCostCalculation_Application/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Gold_SilverCostCalculation_Application
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CostCalculation cc = new CostCalculation();
+            Console.WriteLine("Enter Choice(Gold/Silver)");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "Gold":
+                    Console.WriteLine("Enter grams of Gold Item");
+                    double goldweight = Convert.ToDouble(Console.ReadLine());
+                    cc.GoldCostcalculation(goldweight);
+                    //Console.WriteLine("Total Amount for Gold of weight {0} is {1}", goldweight, totalamount);
+                    break;
+                case "Silver":
+                    Console.WriteLine("Enter grams of Gold Item");
+                    double silverweight = Convert.ToDouble(Console.ReadLine());
+                    cc.SilverCostcalculation(silverweight);
+                    //Console.WriteLine("Total Amount for Silver of weight {0} is {1}", silverweight, totalfare);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice!");
+                    break;
+
+
+
+                    //}
+
+                    //cc.GoldCostcalculation(3);
+                    //Console.WriteLine(amount);
+
+            }
+        }
+    }
+}
+
